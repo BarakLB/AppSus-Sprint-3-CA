4d1beafbb4e9c5256b91c4b1c5e77af29aad7301
BarakLB <Baraktv@gmail.com>
2021-12-30 00:03:16 +0200

    end of day 1

--- js/apps/mail/cmps/MailPreview.jsx
+++ js/apps/mail/cmps/MailPreview.jsx
@@ -1,25 +1,31 @@
-import { utilService } from "../../../services/util.service.js"
-
 const { Link } = ReactRouterDOM
+//SERVICES
+import { utilService } from "../../../services/util.service.js"
+//CMPS
+import { TxtLength } from "./TxtLength.jsx"
 
 
 
 export function MailPreview({ mail }) {
     mail.isRead = true
-    console.log('sadasdasd',mail)
+    console.log('sadasdasd', mail)
     return <section className="mail-preview">
-        <Link className="mail-preview-link clean-link flex align-center justify-center" to={`mail/edit/${mail.id}`}>
-            <div className="sender-name">
-                <p>{mail.nickname}</p>
-            </div>
-            <div className="mail-subject flex justify-between">
-                <p>{mail.subject}</p>
-                <p>{mail.body}</p>
-            </div>
-            <div className="sent-at">
-                <h3>{utilService.handleTimestamp(mail.sentAt)}</h3>
-            </div>
+        <Link className="mail-preview-link clean-link flex align-center space-between" to={`mail/edit/${mail.id}`}>
+            {/* <div className=" "> */}
+                <div className="sender-name">
+                    <p>{mail.nickname}</p>
+                </div>
+                {/* <div className="mail-subject flex justify-between"> */}
+
+                    <p>{mail.subject}</p>
+                    <TxtLength text={mail.body} />
+                    {/* <p>{mail.body}</p> */}
+                {/* </div> */}
+                <div className="sent-at">
+                    <h3>{utilService.handleTimestamp(mail.sentAt)}</h3>
+                </div>
         </Link>
+            {/* </div> */}
     </section>
 
 
